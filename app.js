65bdc14a0afc802cd484bc8a43963d538cd74dab
Elodie mongellaz <mongellaz.e@gmail.com>
2022-12-17 15:14:40 +0100

    returns a JSON response

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/', (req,res) => res.send('Hello again, Express !'));
 app.get('/api/pokemons/:id', (req,res) => {
     const id = parseInt(req.params.id)
     const pokemon = pokemons.find(pokemon => pokemon.id === id)
-    res.send(`Vous avez demandé de pokémon ${pokemon.name}`)
+    res.json(pokemon)
 });
 
 // le nouveau point de terminaison, affichant  le nombre total de pokémons :
