632198770c202415afb5096a7656121f500a5e95
Elodie mongellaz <mongellaz.e@gmail.com>
2022-12-17 13:58:26 +0100

    add new url path based on id on localhost

--- app.js
+++ app.js
@@ -6,5 +6,10 @@ const port = 3000;
 
 app.get('/', (req,res) => res.send('Hello again, Express !'));
 
+app.get('/api/pokemons/:id', (req,res) => {
+    const id = req.params.id
+    const pokemon = pokemons.find(pokemon => pokemon.id === id)
+    res.send(`Vous avez demandé de pokémon ${pokemon.name}`)
+});
 
 app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`));
