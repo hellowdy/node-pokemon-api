36e0c798042ea83dc4139fa136f3556b9975528a
Elodie mongellaz <mongellaz.e@gmail.com>
2022-12-22 14:36:52 +0100

    create middleware logger

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ let pokemons = require('./mock-pokemon');
 const app = express();
 const port = 3000;
 
+const logger = (req, res, next) => {
+    console.log(`URL : ${req.url}`);
+    next();
+};
+
+app.use(logger); 
+
 app.get('/', (req,res) => res.send('Hello again, Express !'));
 
 app.get('/api/pokemons/:id', (req,res) => {
