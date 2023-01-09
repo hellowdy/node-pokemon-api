07ed9c63cc1b8f60214f2c460c70a17d5f66655f
Elodie mongellaz <mongellaz.e@gmail.com>
2023-01-09 18:53:31 +0100

    add getUniqueId

--- helper.js
+++ helper.js
@@ -2,3 +2,11 @@ exports.sucess = (message, data) => {
     return { message, data };
 };
 
+exports.getUniqueId = (pokemons) => {
+    const pokemonsIds = pokemons.map(pokemon => pokemon.id);
+    const maxId = pokemonsIds.reduce((a,b) => Math.max(a,b));
+    const uniqueId = maxId + 1;
+
+    return uniqueId;
+};
+
